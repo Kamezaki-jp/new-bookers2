114feacab2490ebbc78331f00916db1cad82f527
Kamezaki-jp <champagne.es335@gmail.com>
2021-02-24 03:55:22 +0000

    add book edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
     @user = current_user
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -16,27 +16,38 @@ class BooksController < ApplicationController
       render 'index'
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @user = @book.user
     @newbook = Book.new
   end
-  
+
   def edit
-    
+    @book = Book.find(params[:id])
+    if @book.user_id == current_user.id
+      render 'edit'
+    else
+      redirect_to books_path
+    end
   end
-  
+
   def update
-    
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
-  
+
   def destroy
-    
+
   end
-  
+
   private
-  
+
     def book_params
       params.require(:book).permit(:title, :body)
     end
